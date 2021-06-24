20d1913735c70bbd4ea3f9792b74aaaef072e9c9
ConaGo <l_jasper@web.de>
2021-06-24 09:25:20 +0200

    Update Methods_and_Interfaces_2 Errors.go

--- Methods_and_Interfaces_2 Errors.go
+++ Methods_and_Interfaces_2 Errors.go
@@ -19,7 +19,7 @@ func Sqrt(x float64) (float64, error) {
 	_z:= 1.0
 	for i:=0; i<10; i++ {
 		_z -= (z*z - x) / (2*z)
-		if -0.00000000000001<_z-z && _z-z<0.0000000000001 {
+		if -1e-30 < _z-z && _z-z < 1e-30 {
 			return _z, nil
 		}
 	z =_z
